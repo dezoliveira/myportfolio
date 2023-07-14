4f93e7a13e9c148859e70cd3b942656b92f03a03
dezoliveira <andresoliveira@protonmail.com>
2023-07-14 08:43:24 -0300

    dinamic images with ibb

--- script.js
+++ script.js
@@ -69,6 +69,7 @@ const loadProjects = async(data) => {
   const url = data.repos_url
   const response = await fetch(url)
   const repos = await response.json()
+
   let html = ''
 
   html += '<div class="projects">'
@@ -78,14 +79,55 @@ const loadProjects = async(data) => {
   `
 
   for (let i in repos){
-    console.log(repos[i])
+    let projectName = repos[i].name
+    let projectImage = 'https://i.ibb.co/GFvs7qq/default.png'
+
+    if (projectName === 'AgendaJS')
+      projectImage = "https://i.ibb.co/FXhDVGr/AgendaJS.png"
+
+    if (projectName === 'flexblog')
+      projectImage = "https://i.ibb.co/FJ4sHnr/flexblog.png"
+
+    if (projectName === 'javacriptform')
+      projectImage = "https://i.ibb.co/RQGfdD8/javascriptform.png"
+
+    if (projectImage === 'JSCalendar')     
+      projectImage === "https://i.ibb.co/dGhNKt2/JSCalendar.png"
+
+    if (projectName === 'love-calculator')
+      projectImage = "https://i.ibb.co/hcNdHJ5/love-calculator.png"
+
+    if (projectName === 'pizzaria-JS')
+      projectImage = "https://i.ibb.co/ZcQ46nH/pizzaria-JS.png"
+
+    if (projectName === 'pokegenarator')
+      projectImage = "https://i.ibb.co/3cX3S6G/pokegenerator.png"
+
+    if (projectName === 'randommeal')
+      projectImage = "https://i.ibb.co/K01WLCw/randommeal.png"
+
+    if (projectName === 'randompet')
+      projectImage = "https://i.ibb.co/6PVSQdF/randompet.png"
+
+    if (projectName === 'reactmonsters')
+      projectImage = "https://i.ibb.co/D9bFWN1/reactmonsters.png"
+
+    if (projectName === 'simple-portfolio')
+      projectImage = "https://i.ibb.co/x5gsqQv/simple-portfolio.png"
+
+    if (projectName === 'userapi')
+      projectImage = "https://i.ibb.co/XL7hf8V/userapi.png"
+
+    if (projectName === 'vuesouls')
+      projectImage = "https://i.ibb.co/qrYn3k0/vuesouls.png"
+
     html += `
       <div class="card">
         <div class="card-title">
-          <h1>${repos[i].name}</h1>
+          <h1>${projectName}</h1>
         </div>
         <div class="card-body">
-          <img src="https://i.ibb.co/r3j725k/project12.png"/>
+          <img src="${projectImage}"/>
         </div>
       </div>
     ` 
